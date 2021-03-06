feee1eed41f7ad6c019123e7555639c24bd32076
Byung Moon Kim <kimbm2014@gmail.com>
2021-03-06 01:29:12 -0800

    update the launch screen

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class HomeTableViewController: UITableViewController {
 
+    
+    var tweetArray = [NSDictionary]()
+    var numberOfTweet: Int!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadTweet() //when i load the app display the tweet content
+    }
+    
+    func loadTweet(){
+        
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let myParames = ["count": 10]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParames, success: { (tweets: [NSDictionary]) in
+            
+            
+            self.tweetArray.removeAll()  //repopulate the list
+            
+            for tweet in tweets {
+                self.tweetArray.append(tweet)
+            }
+            self.tableView.reloadData()
+            
+        }, failure: { (Error) in
+            print("Could not retreive tweets!")
+        })
+    }
+    
+    
     @IBAction func onLogout(_ sender: Any) {
         TwitterAPICaller.client?.logout()
         self.dismiss(animated: true, completion: nil)
@@ -17,26 +47,20 @@ class HomeTableViewController: UITableViewController {
         UserDefaults.standard.set(false, forKey: "userLoggedIn")  //After I click logout button, this app will not login automatically.
     }
     
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCell
         
-        cell.userNameLabel.text = "Some name"
-        cell.tweetContent.text = "Something else"
+       // let user = tweetArray[indexPath.row]["user"] as! NSDictionary
         
+        //cell.userNameLabel.text = user["name"] as? String
+        cell.userNameLabel.text = "name"
+        //cell.tweetContent.text = tweetArray[indexPath.row]["text"] as! String
         
+        cell.tweetContent.text = "Something"
         return cell
     }
     
-    
-    
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -46,7 +70,7 @@ class HomeTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 5
+        return tweetArray.count
     }
 
 }
